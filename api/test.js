3caf1ff2f5fd5eae57d4be847849bd2de820a5b5
Harsha <harshahy426@gmail.com>
2026-09-20 20:13:26 +0530

    chore(security): gitignore and untrack api folder to prevent exposing keys

--- api/test.js
+++
@@ -1,4 +0,0 @@
-module.exports = (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ status: 'OK', nodeVersion: process.version, envUrl: Boolean(process.env.SUPABASE_URL) }));
-};
